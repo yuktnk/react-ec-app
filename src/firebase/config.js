9148a794f02e1d2349e935528585c9f8f49eada5
yuktnk <yuki0wl@gmail.com>
2020-10-04 22:09:33 +0900

    Firebase用の設定ファイルの作成

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjnVZdhsl03E4Z3tJWcpaYhl1blgRmT2U",
+  authDomain: "yuktnk-ec-app.firebaseapp.com",
+  databaseURL: "https://yuktnk-ec-app.firebaseio.com",
+  projectId: "yuktnk-ec-app",
+  storageBucket: "yuktnk-ec-app.appspot.com",
+  messagingSenderId: "986071352303",
+  appId: "1:986071352303:web:1bd00a4cdc6479ae06ec1a",
+  measurementId: "G-2RJKZJ20G4"
+};

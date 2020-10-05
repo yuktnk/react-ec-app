daa46fd010d0d0bd7c6a9d7de0ed224ab7d679e6
yuktnk <yuki0wl@gmail.com>
2020-10-05 09:08:24 +0900

    サインアップ機能の実装

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,6 +1,6 @@
 import { signInAction } from "./actions";
-import {push} from 'connected-react-router';
-
+import { push } from 'connected-react-router';
+import { auth, db, FirebaseTimestamp } from '../../firebase/index';
 
 
 
@@ -25,5 +25,43 @@ export const signIn = () => {
       }))
       dispatch(push('/'))
     }
+  }
+}
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async (dispatch) => {
+    // Validation
+    if (username === "" || email === "" || password === "" || confirmPassword === "") {
+      alert("必須項目が未入力です")
+      return false
+    }
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません。もう一度お試しください。")
+      return false
+    }
+
+    return auth.createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user;
+
+        if (user) {
+          const uid = user.uid;
+          const timeStamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            uid: uid,
+            username: username,
+            email: email,
+            created_at: timeStamp,
+            updated_at: timeStamp,
+            role: "customer"
+          }
+
+          db.collection('users').doc(uid).set(userInitialData)
+            .then( () => {
+              dispatch(push('/'))
+            })
+        }
+      })
   }
 }
--- src/templates/SignUp.jsx
+++ src/templates/SignUp.jsx
@@ -1,7 +1,11 @@
 import React, { useCallback, useState } from 'react'
 import { PrimaryButton, TextInput } from '../components/UIkit';
+import { signUp } from '../reducks/users/operations';
+import { useDispatch } from 'react-redux';
 
 const SignUp = () => {
+
+  const dispatch = useDispatch()
   
   const [username, setUsername] = useState(""),
         [email, setEmail] = useState(""),
@@ -45,7 +49,7 @@ const SignUp = () => {
       <div className="center">
         <PrimaryButton 
           label={"アカウントを登録する"}
-          onClick={ () => console.log("clicked")}
+          onClick={ () => dispatch(signUp(username, email, password, confirmPassword))}
         />
       </div>
     </div>

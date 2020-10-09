536dc37d8f5583647553e16000d644365f8e639e
yuktnk <yuki0wl@gmail.com>
2020-10-09 15:18:58 +0900

    ファイルの作成と整理

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,4 +1,3 @@
-export { default as Login } from './Login.jsx';
 export { default as Home } from './Home.jsx';
 export { default as SignIn } from './SignIn.jsx';
 export { default as SignUp } from './SignUp.jsx';
